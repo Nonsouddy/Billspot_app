271b7feb0ad1f3d5fa367d82136ffb5644521168
octacondeveloper <kaakadev@gmail.com>
2024-08-21 19:28:13 +0100

    add faq

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="shortcut icon" href="logo.png" />
+      <link rel="shortcut icon" href="/static/logo.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
